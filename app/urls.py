f681df619dcab10b8e8d09f89c3b85be501abe67
VietDung7301 <vietdung.7301@gmail.com>
2023-09-13 16:41:03 +0700

    Task 67758: [Guess-User] Xem chi tiết sản phẩm

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, sign_up
+from .views import HomeView, sign_up, ItemDetailView
 
 app_name = 'app'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('register/', sign_up, name='register'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
 ]
