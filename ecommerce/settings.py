caec64ad5d3205f5dc127e212ce1046f8cf2e982
VietDung <vietdung.7301@gmail.com>
2023-10-02 00:48:03 +0700

    Task 67787 - Gửi email - Base service

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,5 +145,9 @@ AUTH_USER_MODEL = 'app.User'
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = True
